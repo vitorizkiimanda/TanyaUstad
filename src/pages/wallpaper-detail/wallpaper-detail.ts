645c3d1f23911fcc4d61710dfe9d50a95ff7f56e
unknown <vitorizkiimanda@gmail.com>
2018-03-01 11:45:47 +0700

    css fixing home, download plugin not tried yet

--- src/pages/wallpaper-detail/wallpaper-detail.ts
+++ src/pages/wallpaper-detail/wallpaper-detail.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ActionSheetController, LoadingController } from 'ionic-angular';
+import { AuthHttp } from 'angular2-jwt';
+import { Data } from '../../providers/data';
 
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 
 @Component({
   selector: 'page-wallpaper-detail',
@@ -16,7 +19,15 @@ export class WallpaperDetailPage {
   picture3:any;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public alertCtrl: AlertController,
+    public actionSheetCtrl : ActionSheetController,
+    public authHttp: AuthHttp,
+    public data: Data,
+    public loadCtrl: LoadingController,
+    private transfer: FileTransfer) {
 
     let temp = this.navParams.data;
     this.name = temp.name;
@@ -33,5 +44,91 @@ export class WallpaperDetailPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad WallpaperDetailPage');
   }
+
+  
+  downloadImage(data: any){
+    let temp = data;
+    data = 'http://156.67.218.250:81'+data;
+    console.log(data);
+
+    let confirm = this.alertCtrl.create({
+      title: 'Download Gambar?',
+      buttons: [
+        {
+            text: 'Batal',
+            handler: () => {
+              console.log('Disagree clicked');
+            }
+          },
+          {
+            text: 'Download',
+            handler: () => {
+              console.log('Agree clicked');
+
+              // download foto
+
+
+              let loading = this.loadCtrl.create({
+                content: 'download..'
+              });
+          
+              loading.present();
+          
+              setTimeout(() => {
+                loading.dismiss();
+              }, 5000);
+              // api
+          
+              const fileTransfer: FileTransferObject = this.transfer.create();
+          
+              
+              fileTransfer.download(data, temp).then((entry) => {
+                console.log('download complete: ' + entry.toURL());
+
+                let alert = this.alertCtrl.create({
+                  title: 'Download Foto Berhasil',
+                  buttons: [
+                    {
+                      text: 'OK',
+                      handler: () => {
+                        console.log('Agree clicked');
+                      }
+                    }
+                  ]
+                });
+                  alert.present();   
+
+              }, (error) => {
+                // handle error
+                let alert = this.alertCtrl.create({
+                  title: 'Download Foto Gagal',
+                  message: 'silahkan coba kembali',
+                  buttons: [
+                    {
+                      text: 'OK',
+                      handler: () => {
+                        console.log('Agree clicked');
+                      }
+                    }
+                  ]
+                });
+                  alert.present();   
+              });
+          
+             
+              // download foto
+
+
+
+
+          }
+        }
+      ]
+    });
+      confirm.present();    
+
+  }
+
+
 
 }
